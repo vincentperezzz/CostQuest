7764ab4ba2928c4306c7fbe40bd4b6b024b3a126
Vincent Perez <perezvincent100@gmail.com>
2024-11-08 18:22:55 +0800

    update file paths: change icon and stylesheet links to relative paths in HTML and CSS files

--- signup.php
+++ signup.php
@@ -5,17 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=egde:">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> CostQuest - Create an Account </title>
-    <link rel="stylesheet" href="/css/signup.css">
-    <link rel="icon" href="/icons/webicon.png">
+    <link rel="stylesheet" href="css/signup.css">
+    <link rel="icon" href="icons/webicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Reenie+Beanie&display=swap" rel="stylesheet">
-    <script src="/javascript/index.js"> </script>
+    <script src="javascript/index.js"> </script>
     </head>
 <body>   
     <nav class="navbar">
         <div class="max-width">
-        <div class="logo"><a href="index.html"><img src="/icons/logo.png"></a></div>
+        <div class="logo"><a href="index.html"><img src="icons/logo.png"></a></div>
         <ul class="menu">
         <li><a class="menu-btn">Already have an account? </a></li>
         <li><a class="menu-btn-box" href="login.html">Login</a></li>
@@ -25,7 +25,7 @@
         </header>
 <!------------Login Body------------->
     <div class="login-img">
-        <img src="/icons/signup-1st-img.png"></div>
+        <img src="icons/signup-1st-img.png"></div>
     
     <div class="login-box">
       <div class="login-form">
@@ -52,9 +52,21 @@
             <input type="password" placeholder="Confirm Password" name="confirm-password" required>
           </div>
 
-          <input type="submit" class="btn" value="Create an Account">
+            <div class="textbox"></div>
+            <label for="num-people">Number of People Traveling:</label>
+            <select id="num-people" name="num-people" required>
+              <option value="" disabled selected>Select number</option>
+              <?php for ($i = 1; $i <= 100; $i++): ?>
+              <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+              <?php endfor; ?>
+            </select>
+            </div>
+
+          <input type="submit" class="btn" value="Continue Signing Up">
         </form>
       </div>
+
+      
     </div>
 
 <!------------Footer------------->
