ed2e52084fa1b84fbfe80b3499872528e7237e4e
Vincent Perez <perezvincent100@gmail.com>
2024-11-10 13:25:16 +0800

    fix: update users table to set email as primary key and remove id column

--- database.sql
+++ database.sql
@@ -3,10 +3,9 @@ CREATE DATABASE costquest;
 USE costquest;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    email VARCHAR(255),
+    email VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255),
     num_people INT,
     budget DECIMAL(10, 2)
