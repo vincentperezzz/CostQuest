101679e0441d693312f28662181a2200eda7cb5c
Vincent Perez <perezvincent100@gmail.com>
2024-11-14 00:43:08 +0800

    feat: enhance account settings UI with new layout and buttons; add email and password change forms

--- settings.php
+++ settings.php
@@ -67,28 +67,32 @@
 
 
 <div class="login-box">
-            <h1>Edit account details here!</h1>
+            <h6>Account Details ></h6>
+            <div class="title-row">
+                <h1>Account Details</h1>
+                <div class="del-btn"> Delete Account </div>
+            </div>
+
             <form action="settings.php" method="post" onsubmit="toggle_Continue_SigningUp(event)">
                 <div class="name-row">
-                    <div class="textbox">
-                        <input type="text" placeholder="<?php echo $first_name ?>" name="fname">
+                    <div class="non-textbox" style="margin-right: 10px;">
+                        <?php echo $first_name ?>
                     </div>
-                    <div class="textbox">
-                        <input type="text" placeholder="<?php echo $last_name ?>" name="lname">
+                    <div class="non-textbox">
+                        <?php echo $last_name ?>
                     </div>
                 </div>
-        
-                <div class="textbox">
-                    <input type="email" placeholder="<?php echo $email ?>" name="email">
-                </div>
-        
-                <div class="textbox">
-                    <input type="password" placeholder="Modify Password" name="password" required>
-                </div>
-        
-                <div class="textbox">
-                    <input type="password" placeholder="Confirm Password" name="confirm-password" required>
-                </div>
+                <div class="non-textbox">
+                    <?php echo $email ?>
+                    <div class="edit-btn" id="editEmailButton" onclick="editEmail()">change</div>
+                </div>        
+
+                <div class="non-textbox">
+                    Password
+                    <div class="edit-btn" id="editPasswordButton" onclick="editPassword()">change</div>
+                </div>  
+
+
 
                 <div class="name-row">
                     <div class="textbox">
@@ -100,12 +104,65 @@
                         </select>
                     </div>
                 </div>
-        
-                <button type="submit" class="btn">Continue</button>
-                <button type="submit" class="logout-btn" onclick="window.location.href='index.html'">Logout</button>
+                <button type="submit" class="logout-btn" onclick="window.location.href='index.html'">Logout</button>               
             </form>
-            
-  </div>
+ </div>
+
+ <div class="email-box">
+            <h6>Account Details > Change Email</h6>
+                <h1>Change Email</h1>
+
+            <form action="settings.php" method="post" onsubmit="toggle_Continue_SigningUp(event)">
+                <div class="textbox">
+                    <input type="email" placeholder="Old Email" name="old-email" required>
+                </div>      
+
+                <div class="textbox">
+                    <input type="text" placeholder="New Email" name="new-email" required>
+                </div> 
+                
+                <div class="textbox">
+                    <input type="password" placeholder="Password" name="password" required>
+                </div> 
+
+                <div class="btn-row">
+                    <div>
+                        <button type="submit" class="cancel-btn">Cancel</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="confirm-btn">Confirm</button>
+                    </div>
+                </div>            
+            </form>
+ </div>
+
+ <div class="password-box">
+            <h6>Account Details > Change Password</h6>
+                <h1>Change Password</h1>
+
+            <form action="settings.php" method="post" onsubmit="toggle_Continue_SigningUp(event)">
+                <div class="textbox">
+                    <input type="password" placeholder="Old password" name="old-password" required>
+                </div>      
+
+                <div class="textbox">
+                    <input type="password" placeholder="New password" name="new-password" required>
+                </div> 
+                
+                <div class="textbox">
+                    <input type="password" placeholder="Confirm New Password" name="confirm-new-password" required>
+                </div> 
+
+                <div class="btn-row">
+                    <div>
+                        <button type="submit" class="cancel-btn">Cancel</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="confirm-btn">Confirm</button>
+                    </div>
+                </div>            
+            </form>
+ </div>
     
  
 
