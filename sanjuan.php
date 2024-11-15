a2d823696d9ec90a3cdfa49aa1a0079aa04f7342
Vincent Perez <perezvincent100@gmail.com>
2024-11-15 10:51:47 +0800

    feat: add itineraries budget section and update itinerary button labels

--- sanjuan.php
+++ sanjuan.php
@@ -47,7 +47,7 @@ $result = $conn->query($sql);
             <div class="logo"><a href="dashboard.php"><img src="icons/logo.png"></a></div>
 
             <div class="itinerary">
-                <div class="itinerary-btn">Itinerary</div>
+                <div class="itinerary-btn">Itinerary Cart</div>
                 <div class="itinerary-btn-box"><a href="itineraries.php"><img class="loc-ico" src="icons/itineraries-location-icon.png">0</a></div>
             </div>
         </div>
